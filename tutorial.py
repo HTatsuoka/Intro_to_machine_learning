f619c27b7f53a717c99bf084739af65471527709
Tatsuoka <hisatotatsuoka@gmail.com>
2022-10-10 09:44:30 +0900

    pycharm更新

--- tutorial.py
+++ tutorial.py
@@ -9,5 +9,32 @@ print(home_data.head())
 print(home_data.describe())
 
 print(home_data.columns)
+##
+y = home_data.SalePrice
 
+feature_names = ["LotArea", "YearBuilt", "1stFlrSF", "2ndFlrSF", "FullBath", "BedroomAbvGr", "TotRmsAbvGrd"]
+
+X = home_data[feature_names]
+
+X.describe()
+##
+X.head()
+##
 from sklearn.tree import DecisionTreeRegressor
+
+iowa_model = DecisionTreeRegressor(random_state=1)
+
+iowa_model.fit(X, y)
+
+predictions = iowa_model.predict(X)
+print(predictions)
+##
+y.head()
+##
+from sklearn.model_selection import train_test_split
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+print(len(train_X))
+
+print(len(val_X))
